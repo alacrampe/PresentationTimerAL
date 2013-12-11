6f3ac9dc9ac554692027853f36e27024443a9366
DimitriHeloin <dimitri.heloin@ingesup.com>
2013-12-11 11:15:43 +0100

    Ressource zonetexte

--- gen/com/example/presentationtimer/R.java
+++ gen/com/example/presentationtimer/R.java
@@ -24,6 +24,7 @@ public final class R {
         public static final int background=0x7f020000;
         public static final int createpresent=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int zonetexte=0x7f020003;
     }
     public static final class id {
         public static final int AddTask=0x7f080002;
