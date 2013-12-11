c021d1ef444a213899a1f420f00b899bb43bf24b
DimitriHeloin <dimitri.heloin@ingesup.com>
2013-12-11 11:35:03 +0100

    Activity_create mise a jour
    + ressources fondListe

--- gen/com/example/presentationtimer/R.java
+++ gen/com/example/presentationtimer/R.java
@@ -23,20 +23,23 @@ public final class R {
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int createpresent=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int zonetexte=0x7f020003;
+        public static final int fondliste=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int plus=0x7f020004;
+        public static final int zonetexte=0x7f020005;
     }
     public static final class id {
         public static final int AddTask=0x7f080002;
         public static final int Name=0x7f080000;
-        public static final int StartButton=0x7f080007;
+        public static final int StartButton=0x7f080008;
         public static final int Submit=0x7f080004;
         public static final int TaskList=0x7f080003;
         public static final int Time=0x7f080001;
-        public static final int action_settings=0x7f080009;
-        public static final int name=0x7f080005;
-        public static final int progressBar1=0x7f080006;
-        public static final int time=0x7f080008;
+        public static final int action_settings=0x7f08000a;
+        public static final int imageView1=0x7f080005;
+        public static final int name=0x7f080006;
+        public static final int progressBar1=0x7f080007;
+        public static final int time=0x7f080009;
     }
     public static final class layout {
         public static final int activity_create=0x7f030000;
