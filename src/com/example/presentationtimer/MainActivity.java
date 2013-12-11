514718121abbe584daeac02a5537b9aef25a5c88
Arnaud Lacrampe <arnaud.lacrampe@gmail.com>
2013-12-11 14:14:06 +0100

    Menu: create item working

--- src/com/example/presentationtimer/MainActivity.java
+++ src/com/example/presentationtimer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.presentationtimer;
 
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
@@ -10,22 +11,13 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 	public Button create;
+	public ActionBar actionBar;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		create=(Button) this.findViewById(R.id.create);
 		
-		create.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent intent=new Intent(MainActivity.this,CreateActivity.class);
-				startActivity(intent);
-			}
-		});
 		
 	}
 
